53f5680dbf71a03725077db1b9d964f93fc9c679
Marc Berengueras <mberengueras@nexusgeographics.com>
2021-12-30 19:42:59 +0100

    Actualitzacio Desembre 2021

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,6 @@
+const BASE_URL = "https://us-central1-accesscontrol-f2410.cloudfunctions.net/";
+const EDIT_USER_URL = BASE_URL + "editUser";
+
 var Login = new Vue ({
 	el: "#loginPopup",
 	data: {
@@ -8,6 +11,7 @@ var Login = new Vue ({
 		phone: "",
 		errorMsg: "",
 		signUpErrorMsg: "",
+		loginTitle: "Inicia la sessió",
 		displayEmailForm: true,
 		displaySignUpForm: false
 	},
@@ -27,9 +31,11 @@ var Login = new Vue ({
 			});
 	    },
 	    passwordRecovery: function () {
+	    	this.loginTitle = "Recupera la contrasenya";
 			this.displayEmailForm = false;
 	    },
 	    cancelPasswordRecovery: function () {
+	    	this.loginTitle = "Inicia la sessió";
 			this.displayEmailForm = true;
 	    },
 	    sendPasswordRecovery: function () {
@@ -41,6 +47,7 @@ var Login = new Vue ({
 		    });
 	    },
 	    showSignUpForm: function () {
+	    	this.loginTitle = "Registra't";
 			this.displaySignUpForm = true;
 	    },
 	    signUp: function () {
@@ -49,15 +56,37 @@ var Login = new Vue ({
 	    		return;
 	    	}
 
-	    	var self = this;
-			firebase.auth().createUserWithEmailAndPassword(this.email, this.password).catch(function(error) {
-			  // Handle Errors here.
-			  var errorCode = error.code;
-			  self.signUpErrorMsg = error.message;
-			  // ...
+			firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(() => {
+				firebase.auth().currentUser.getIdToken(true).then((idToken) => {
+					var userData = {"displayName": this.name, "phone": this.phone, "email": this.email}
+				  	// Send token to your backend via HTTPS
+					$.ajax({
+					  type: "POST",
+					  url: EDIT_USER_URL,
+					  headers: {"Authorization" : "Bearer " + idToken},
+					  data: userData,
+					  success: function(res) {
+					  	var url = window.location.origin + "/reserves.html";
+      		  			window.location = url;
+					  },
+					  error: function(err) {
+					  	console.log(err)
+					  	this.signUpErrorMsg = "S'ha produït un error";
+					  },
+					  dataType: "application/json"
+					});
+				}).catch((error) => {
+			 		console.log(error)
+				});
+			}).catch((error) => {
+			    console.log(error)
+				// Handle Errors here.
+				var errorCode = error.code;
+				this.signUpErrorMsg = error.message;
 			});
 	    },
 	    cancelSignUp: function () {
+	    	this.loginTitle = "Inicia la sessió";
 			this.displaySignUpForm = false;
 	    },
 	    errorLabelDisplay: function(){
@@ -66,10 +95,54 @@ var Login = new Vue ({
 	}
 });
 
+// target elements with the "draggable" class
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
+    // enable autoScroll
+    autoScroll: true,
+
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {
+      var textEl = event.target.querySelector('p');
+
+      textEl && (textEl.textContent =
+        'moved a distance of '
+        + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                     Math.pow(event.pageY - event.y0, 2) | 0))
+            .toFixed(2) + 'px');
+    }
+  });
+
+  function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+}
+
+
 
 var updateInstrSize = function(){
 	var intrWidth = parseInt($(".img-instructions").css("width").replace("px",""));
-	console.log(intrWidth);
     $(".img-instructions").css("height", Math.round(intrWidth*0.5625));
 }
 
@@ -79,4 +152,5 @@ $(window).on('resize', function(){
 
 $(document).ready(function() {
 	updateInstrSize();
+	window.dragMoveListener = dragMoveListener;
 });
